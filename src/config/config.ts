4ebe969efc919ca342c198bf5be04bda9b2739b4
probatorem-h1 <hackermanresurrected@gmail.com>
2022-05-20 18:48:00 -0500

    commit

--- src/config/config.ts
+++ src/config/config.ts
@@ -24,8 +24,8 @@ export const configVars = {
     address_mainnet: "0x0CA0b3f8B2Fe3dC6B8BA77955793C0aB7e9BDD89", 
   },
   discordLink: {
-    address: "0x8F14cFecc6B51100F1D482FADE23442DEc414Dec", 
-    address_mainnet: "0x8F14cFecc6B51100F1D482FADE23442DEc414Dec", 
+    address: "0xf7e7E79970657689A861a3DB1dE297089e042D12", 
+    address_mainnet: "0xf7e7E79970657689A861a3DB1dE297089e042D12", 
   },
   wETH: {
     address: "0x757c86b41E5048B99311Ea55f37A5f116b3D42eA", 
