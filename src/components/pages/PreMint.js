0dd3496a3331dde27ff4fc6b819edf9e90847136
probatorem-h1 <hackermanresurrected@gmail.com>
2022-06-09 16:04:36 -0700

    commit

--- src/components/pages/PreMint.js
+++ src/components/pages/PreMint.js
@@ -322,6 +322,7 @@ function PreMint() {
                                     <h3 style={{ color: 'white' }}>How to Install Metamask:</h3>
                                     Download Metamask Wallet: <a href="https://metamask.io/download/" target='_blank' rel="noreferrer" style={{ color: 'white' }} >https://metamask.io/download/</a> <br />
                                     Video for installing Metamask on Chrome: <a href="https://www.youtube.com/watch?v=OJqaZRpRqXM" target='_blank' rel="noreferrer" style={{ color: 'white' }} >https://www.youtube.com/watch?v=OJqaZRpRqXM </a><br />
+                                    Adding Polygon Mainnet to Metamask: <a href="https://docs.polygon.technology/docs/develop/metamask/config-polygon-on-metamask/" target='_blank' rel="noreferrer" style={{ color: 'white' }} >https://docs.polygon.technology/docs/develop/metamask/config-polygon-on-metamask/</a><br />
                                     Using Metamask to pay with Polygon/Matic: <a href="https://gravityfinance.medium.com/using-metamask-with-polygon-923f061054db" target='_blank' rel="noreferrer" style={{ color: 'white' }} >https://gravityfinance.medium.com/using-metamask-with-polygon-923f061054db</a>
 
                                 </BodyText>
